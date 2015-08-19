90e9381886c60f7b597bd7a17d5f2cfe383fcc95
programhung <superhung220703@gmail.com>
2015-08-19 22:26:15 +0200

    Update test_mystery_word.py

--- test_mystery_word.py
+++ test_mystery_word.py
@@ -1,46 +1,38 @@
-from mystery_word import *
-
-word_list = ["bird", "calf", "river", "stream", "kneecap",  "cookbook",
-             "language", "sneaker", "algorithm", "integration", "brain"]
-
-
-def test_easy_words():
-    assert easy_words(word_list) == \
-        ["bird", "calf", "river", "stream", "brain"]
-
-
-def test_medium_words():
-    assert medium_words(word_list) == \
-        ["stream", "kneecap", "cookbook", "language", "sneaker"]
-
-
-def test_hard_words():
-    assert hard_words(word_list) == \
-        ["cookbook", "language", "algorithm", "integration"]
-
-
-def test_random_word():
-    """This test is not very good. Testing things that are random is hard, in
-    that there's not a predictable choice. The best we can do is make sure
-    we have valid output."""
-    word = random_word(word_list)
-    assert word in word_list
-
-
-def test_display_word():
-    word = "integration"
-    assert display_word(word, []) == "_ _ _ _ _ _ _ _ _ _ _"
-    assert display_word(word, ["z"]) == "_ _ _ _ _ _ _ _ _ _ _"
-    assert display_word(word, ["g"]) == "_ _ _ _ G _ _ _ _ _ _"
-    assert display_word(word, ["i"]) == "I _ _ _ _ _ _ _ I _ _"
-    assert display_word(word, ["i", "g"]) == "I _ _ _ G _ _ _ I _ _"
-    assert display_word(word, ["i", "n", "z"]) == "I N _ _ _ _ _ _ I _ N"
-
-
-def test_is_word_complete():
-    word = "river"
-    assert not is_word_complete(word, [])
-    assert not is_word_complete(word, ["r"])
-    assert not is_word_complete(word, ["r", "e"])
-    assert not is_word_complete(word, ["r", "e", "z"])
-    assert is_word_complete(word, ["r", "e", "v", "i"])
+# Guess a word
+# Import library
+import random
+# Make list of word
+Word=['buy','dolphin','machine','mouse','heart','lamp','clock','table','key','water','fire','orange','house','horse']
+choose=random.choice(Word)
+# The length of the secret word
+blanks='_ '*len(choose)
+print("Secret word: "+str(blanks))
+#Guess the letter
+guess=input("Guess the letter of the secret word: ")
+# Make a loop infinity
+x=100000000000000000000
+# Make the time for count
+counter=0
+def whi(guess):
+        while len(guess)>1 or guess not in 'qwertyuiopasdfghjklzxcvbnm':
+                if len(guess)>1:
+                        print("Guess only a letter in one time.")
+                if guess not in 'qwertyuiopasdfghjklzxcvbnm':
+                        print("Guess only the letter and no other.")
+                guess=input("Guess a letter of the secret word: ")
+        return guess
+for letter in range(x):
+        whi(guess)
+        if guess in choose:
+                if counter==len(choose)-1:
+                        print("Congratulation!You won!")
+                        break
+                counter+=1
+                letterIndex=choose.index(guess)
+                newBlanks=blanks[:letterIndex*2] + guess + blanks[letterIndex*2+1:]
+                newBlanks=blanks
+                blanks=blanks[:letterIndex*2] + guess + blanks[letterIndex*2+1:]
+                print("Secret word: "+str(blanks))
+        else:
+                print("It's wrong!")
+        guess=input("Guess again the letter: ")
